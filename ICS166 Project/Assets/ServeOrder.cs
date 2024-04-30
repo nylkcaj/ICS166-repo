160aea6bb6e502ba8c2e1049721ccd38bc5608b0
vaninera <peachmelody6@gmail.com>
2024-04-30 00:48:51 -0700

    Food order is draggable

---
+++ ICS166 Project/Assets/ServeOrder.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ServeOrder : MonoBehaviour
+{
+    private bool dragging = false;
+    private Vector3 offset;
+
+    void Update()
+    {
+        if (dragging)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        dragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        dragging = false;
+    }
+}
